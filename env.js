68616ecc363b162b463753e52d1622db41d67eb4
Irenne Dwita Natalia <93325853+Irennedwita@users.noreply.github.com>
2023-10-01 21:59:59 +0700

    Kumpul Tugas Kecil 2

---
+++ env.js
@@ -0,0 +1,13 @@
+// 1. Membuat Env Variable sesuai data yang diregister
+const cat_env = {
+  API_KEY: "live_dMcsvkpt4DbmLQTPUeyNJPrGm1KcjgeQsgvZYl9ugUDs2hr14wEKRklFa0MH4Rxi",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+const dog_env = {
+  API_KEY: "live_MMbSmWVE22QgH64dAphYLVSQ2uW6NuuhbWOWp3nLtll2zukFPI5wpYfdeDW4C6eJ",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+// 2. Export dog_env
+export default dog_env;
